d5bb151a8d32d358b57cb88993403b1332ba4876
CaioNovoaA <caio.antunes@zup.com.br>
2022-09-25 18:16:04 -0300

    Ajustando camel Case

--- app/src/main/java/br/com/caio/minha_calc/MainActivity.kt
+++ app/src/main/java/br/com/caio/minha_calc/MainActivity.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    var numero1:Double = 0.0
+    var numberOne:Double = 0.0
     var operadorType : Int = 0
     lateinit var lt_Resultado: TextView
     lateinit var lt_Expressao: TextView
@@ -23,14 +23,14 @@ class MainActivity : AppCompatActivity() {
 
 
         botaoigual.setOnClickListener {
-            var numero2: Double = lt_Expressao.text.toString().toDouble()
+            var numberTwo: Double = lt_Expressao.text.toString().toDouble()
 
             var resp: Double = 0.0
             when(operadorType){
-                1-> resp = numero1 + numero2
-                2 -> resp = numero1 - numero2
-                3 -> resp = numero1 * numero2
-                4 -> resp = numero1 / numero2
+                1-> resp = numberOne + numberTwo
+                2 -> resp = numberOne - numberTwo
+                3 -> resp = numberOne * numberTwo
+                4 -> resp = numberOne / numberTwo
             }
             lt_Resultado.setText("")
             lt_Expressao.setText("")
@@ -39,52 +39,52 @@ class MainActivity : AppCompatActivity() {
     botaoapagador.setOnClickListener {
             lt_Resultado.setText("")
             lt_Expressao.setText("")
-            numero1 = 0.0
+            numberOne = 0.0
             operadorType = 0
          }
     }
 
     fun onPress(view: View){
        // val tv_num2: TextView = findViewById(R.id.resultado)
-        var num2: String = lt_Expressao.text.toString()
+        var numTwo: String = lt_Expressao.text.toString()
 
         when(view.id){
-            R.id.numero_Zero -> lt_Expressao.setText(num2+ "0")
-            R.id.numero_Um -> lt_Expressao.setText(num2+"1")
-            R.id.numero_Dois -> lt_Expressao.setText(num2+"2")
-            R.id.numero_Tres -> lt_Expressao.setText(num2+"3")
-            R.id.numero_Quatro -> lt_Expressao.setText(num2+"4")
-            R.id.numero_Cinco -> lt_Expressao.setText(num2+"5")
-            R.id.numero_Seis -> lt_Expressao.setText(num2+"6")
-        R.id.numero_Sete -> lt_Expressao.setText(num2+"7")
-            R.id.numero_Oito -> lt_Expressao.setText(num2+"8")
-            R.id.numero_Nove -> lt_Expressao.setText(num2+"9")
-            R.id.btn_Ponto -> lt_Expressao.setText(num2+".")
+            R.id.numero_Zero -> lt_Expressao.setText(numTwo+ "0")
+            R.id.numero_Um -> lt_Expressao.setText(numTwo+"1")
+            R.id.numero_Dois -> lt_Expressao.setText(numTwo+"2")
+            R.id.numero_Tres -> lt_Expressao.setText(numTwo+"3")
+            R.id.numero_Quatro -> lt_Expressao.setText(numTwo+"4")
+            R.id.numero_Cinco -> lt_Expressao.setText(numTwo+"5")
+            R.id.numero_Seis -> lt_Expressao.setText(numTwo+"6")
+        R.id.numero_Sete -> lt_Expressao.setText(numTwo+"7")
+            R.id.numero_Oito -> lt_Expressao.setText(numTwo+"8")
+            R.id.numero_Nove -> lt_Expressao.setText(numTwo+"9")
+            R.id.btn_Ponto -> lt_Expressao.setText(numTwo+".")
             }
         }
     fun clickOperation(view: View){
-        numero1 = lt_Expressao.text.toString().toDouble()
-        var num2_text: String = lt_Expressao.text.toString()
+        numberOne = lt_Expressao.text.toString().toDouble()
+        var num2_Text: String = lt_Expressao.text.toString()
         when(view.id){
             R.id.operador_soma ->{
-                lt_Resultado.setText(num2_text+ "+")
+                lt_Resultado.setText(num2_Text+ "+")
                 lt_Expressao.setText("")
                 operadorType = 1
             }
             R.id.calculador_menos -> {
-                lt_Resultado.setText(num2_text+ "-")
+                lt_Resultado.setText(num2_Text+ "-")
                 lt_Expressao.setText("")
                 operadorType = 2
             }
             R.id.calc_multiplicador -> {
-                lt_Resultado.setText(num2_text+ "*")
+                lt_Resultado.setText(num2_Text+ "*")
                 lt_Expressao.setText("")
                 operadorType = 3
 
             }
 
             R.id.calc_dividir ->{
-                lt_Resultado.setText(num2_text+ "/")
+                lt_Resultado.setText(num2_Text+ "/")
                 lt_Expressao.setText("")
                 operadorType = 4
             }
