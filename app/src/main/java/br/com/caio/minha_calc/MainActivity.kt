a14888ea01b64c2316af7b62816429df9f38f4f7
CaioNovoaA <caio.antunes@zup.com.br>
2022-10-11 01:06:44 -0300

    ajuste camel case

--- app/src/main/java/br/com/caio/minha_calc/MainActivity.kt
+++ app/src/main/java/br/com/caio/minha_calc/MainActivity.kt
@@ -10,20 +10,20 @@ class MainActivity : AppCompatActivity() {
 
     var numberOne:Double = 0.0
     var operadorType : Int = 0
-    lateinit var lt_Resultado: TextView
-    lateinit var lt_Expressao: TextView
+    lateinit var ltResultado: TextView
+    lateinit var ltExpressao: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        lt_Resultado = findViewById(R.id.resultado)
-        lt_Expressao = findViewById(R.id.lt_expressao)
+        ltResultado = findViewById(R.id.resultado)
+        ltExpressao = findViewById(R.id.lt_expressao)
         val botaoapagador: Button = findViewById(R.id.botaoapagar)
-        val botaoigual: Button = findViewById(R.id.btn_igual)
+        val botaoigual: Button = findViewById(R.id.btnIgual)
 
 
         botaoigual.setOnClickListener {
-            var numberTwo: Double = lt_Expressao.text.toString().toDouble()
+            var numberTwo: Double = ltExpressao.text.toString().toDouble()
 
             var resp: Double = 0.0
             when(operadorType){
@@ -32,13 +32,13 @@ class MainActivity : AppCompatActivity() {
                 3 -> resp = numberOne * numberTwo
                 4 -> resp = numberOne / numberTwo
             }
-            lt_Resultado.setText("")
-            lt_Expressao.setText("")
-            lt_Expressao.setText(resp.toString())
+            ltResultado.setText("")
+            ltExpressao.setText("")
+            ltExpressao.setText(resp.toString())
         }
     botaoapagador.setOnClickListener {
-            lt_Resultado.setText("")
-            lt_Expressao.setText("")
+            ltResultado.setText("")
+            ltExpressao.setText("")
             numberOne = 0.0
             operadorType = 0
          }
@@ -46,46 +46,46 @@ class MainActivity : AppCompatActivity() {
 
     fun onPress(view: View){
        // val tv_num2: TextView = findViewById(R.id.resultado)
-        var numTwo: String = lt_Expressao.text.toString()
+        var numTwo: String = ltExpressao.text.toString()
 
         when(view.id){
-            R.id.numero_Zero -> lt_Expressao.setText(numTwo+ "0")
-            R.id.numero_Um -> lt_Expressao.setText(numTwo+"1")
-            R.id.numero_Dois -> lt_Expressao.setText(numTwo+"2")
-            R.id.numero_Tres -> lt_Expressao.setText(numTwo+"3")
-            R.id.numero_Quatro -> lt_Expressao.setText(numTwo+"4")
-            R.id.numero_Cinco -> lt_Expressao.setText(numTwo+"5")
-            R.id.numero_Seis -> lt_Expressao.setText(numTwo+"6")
-        R.id.numero_Sete -> lt_Expressao.setText(numTwo+"7")
-            R.id.numero_Oito -> lt_Expressao.setText(numTwo+"8")
-            R.id.numero_Nove -> lt_Expressao.setText(numTwo+"9")
-            R.id.btn_Ponto -> lt_Expressao.setText(numTwo+".")
+            R.id.numeroZero -> ltExpressao.setText(numTwo+ "0")
+            R.id.numeroUm -> ltExpressao.setText(numTwo+"1")
+            R.id.numeroDois -> ltExpressao.setText(numTwo+"2")
+            R.id.numeroTres -> ltExpressao.setText(numTwo+"3")
+            R.id.numeroQuatro -> ltExpressao.setText(numTwo+"4")
+            R.id.numeroCinco -> ltExpressao.setText(numTwo+"5")
+            R.id.numeroSeis -> ltExpressao.setText(numTwo+"6")
+        R.id.numeroSete -> ltExpressao.setText(numTwo+"7")
+            R.id.numeroOito -> ltExpressao.setText(numTwo+"8")
+            R.id.numeroNove -> ltExpressao.setText(numTwo+"9")
+            R.id.btnPonto -> ltExpressao.setText(numTwo+".")
             }
         }
     fun clickOperation(view: View){
-        numberOne = lt_Expressao.text.toString().toDouble()
-        var num2_Text: String = lt_Expressao.text.toString()
+        numberOne = ltExpressao.text.toString().toDouble()
+        var numerodoisText: String = ltExpressao.text.toString()
         when(view.id){
-            R.id.operador_soma ->{
-                lt_Resultado.setText(num2_Text+ "+")
-                lt_Expressao.setText("")
+            R.id.operadorSoma ->{
+                ltResultado.setText(numerodoisText+ "+")
+                ltExpressao.setText("")
                 operadorType = 1
             }
-            R.id.calculador_menos -> {
-                lt_Resultado.setText(num2_Text+ "-")
-                lt_Expressao.setText("")
+            R.id.operadorMenos -> {
+                ltResultado.setText(numerodoisText+ "-")
+                ltExpressao.setText("")
                 operadorType = 2
             }
-            R.id.calc_multiplicador -> {
-                lt_Resultado.setText(num2_Text+ "*")
-                lt_Expressao.setText("")
+            R.id.operadorMultiplicacao -> {
+                ltResultado.setText(numerodoisText+ "*")
+                ltExpressao.setText("")
                 operadorType = 3
 
             }
 
-            R.id.calc_dividir ->{
-                lt_Resultado.setText(num2_Text+ "/")
-                lt_Expressao.setText("")
+            R.id.operadorDivisao ->{
+                ltResultado.setText(numerodoisText+ "÷")
+                ltExpressao.setText("")
                 operadorType = 4
             }
         }
